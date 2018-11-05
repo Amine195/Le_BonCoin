71fcf49671f70657a71c001e33e6d9c93fb6c34c
Amine195 <gmadhgs@gmail.com>
2018-11-05 14:42:28 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// Required NPM
+const path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+// Init App
+const app = express();
+
+// Bring Middleware
+//app.use(morgan('tiny'));
+
+// Bring EJS Template
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Bring Routes
+const productRoutes = require('./routes/product');
+const userRoutes = require('./routes/user');
+
+// Static Folder
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')));
+
+// Using Routes
+app.use('/product', productRoutes);
+app.use('/user', userRoutes);
+
+// Server Listening
+app.listen(3000, ()=> {
+    console.log(`Server Running on PORT ${chalk.green('3000')}`);
+});
---
+++ routes/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('product/index');
+});
+
+router.get('/create', (req, res) => {
+    res.render('product/create');
+});
+
+router.get('/show', (req, res) => {
+    res.render('product/show');
+});
+
+router.get('/edit', (req, res) => {
+    res.render('product/edit');
+});
+
+module.exports = router
