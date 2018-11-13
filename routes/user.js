a771b1679a15491c8eb72267ee206a6d9a6acc5f
Amine195 <gmadhgs@gmail.com>
2018-11-13 14:06:11 +0100

    menage

--- routes/user.js
+++ routes/user.js
@@ -8,25 +8,4 @@ const UserController = require('../controllers/userController');
 // Routes
 router.get("/profile", UserController.GetProfile); // Profile
 
-// Forgot Route
-router.get("/forgot", (req, res) => {
-    res.render("user/forgot", {
-        PageTitle: "Forgot Page"
-    });
-});
-
-// Reset Route
-router.get("/reset", (req, res) => {
-    res.render("user/reset", {
-        PageTitle: "Reset Page"
-    });
-});
-
-// Resend Route
-router.get("/resend", (req, res) => {
-    res.render("user/resend", {
-        PageTitle: "Resend Page"
-    });
-});
-
 module.exports = router;
