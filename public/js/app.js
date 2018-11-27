80f71c50fe225dde0c88f38dec4aeb1e1fb365d9
Amine195 <gmadhgs@gmail.com>
2018-11-27 22:38:06 +0100

    grid quick view

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
+// Mdb Validation
+(function () {
+    window.addEventListener("load", () => {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.getElementsByClassName("needs-validation");
+        // Loop over them and prevent submission
+        const validation = Array.prototype.filter.call(forms, (form) => {
+        form.addEventListener("submit", (event) => {
+            if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+        }, false);
+        });
+    }, false);
+}());
 
+// Material Select Initialization
+$(document).ready(function() {
+    $('.mdb-select').materialSelect();
+});
