614db328590bdbd21bbdae5bee6c3d54e2af6717
Amine195 <gmadhgs@gmail.com>
2018-12-03 12:04:15 +0100

    delete error font

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dis
 app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
 app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist")));
 app.use("/js", express.static(path.join(__dirname, "/node_modules/popper.js/dist/umd")));
-app.use("/css", express.static(path.join(__dirname, "/node_modules/@fortawesome/fontawesome-free/css")));
-app.use("/js", express.static(path.join(__dirname, "/node_modules/@fortawesome/fontawesome-free/js")));
-
+app.use("/font", express.static(path.join(__dirname, "/node_modules/@fortawesome/fontawesome-free")));
 
 // Using Routes
 app.get("/", (req, res) => {
