593d02bb3a7c5c0b1ceb9827ddfb6160aaa147b5
Amine195 <gmadhgs@gmail.com>
2018-12-13 21:45:39 +0100

    dashboard

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,38 @@
 exports.GetDashboard = (req, res) => {
     res.render("user/dashboard", {
         path: "/dashboard",
-        PageTitle: "Dashboard Page"
+        PageTitle: "My Dashboard Page"
     });
 }
 
 // GET Profile Controller
 exports.GetProfile = (req, res) => {
     res.render("user/profile", {
-        path: "/profile",
-        PageTitle: "Profile Page"
+        path: "/dashboard",
+        PageTitle: "My Profile Page"
+    });
+}
+
+// GET Product Controller
+exports.GetProduct = (req, res) => {
+    res.render("user/product", {
+        path: "/dashboard",
+        PageTitle: "My Product Page"
+    });
+}
+
+// GET Shop Controller
+exports.GetShop = (req, res) => {
+    res.render("user/shop", {
+        path: "/dashboard",
+        PageTitle: "My Shop Page"
+    });
+}
+
+// GET Announce Controller
+exports.GetAnnounce = (req, res) => {
+    res.render("user/announce", {
+        path: "/dashboard",
+        PageTitle: "My Announce Page"
     });
 }
--- routes/user.js
+++ routes/user.js
@@ -8,5 +8,8 @@ const UserController = require('../controllers/userController');
 // Routes
 router.get("/dashboard", UserController.GetDashboard); // Dashboard
 router.get("/profile", UserController.GetProfile); // Profile
+router.get("/product", UserController.GetProduct); // Product
+router.get("/shop", UserController.GetShop); // Shop
+router.get("/announce", UserController.GetAnnounce); // Announce
 
 module.exports = router;
